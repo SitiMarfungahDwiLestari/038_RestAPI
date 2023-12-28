4bb688a70e9d60253f6a806ac1e03b1dacceabaf
Siti Marfungah Dwi Lestari <siti.marfungah.ft21@mail.umy.ac.id>
2023-12-28 20:41:21 +0700

    membuat data class insert ui event

--- app/src/main/java/com/example/consumerestapi/ui/kontak/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/consumerestapi/ui/kontak/viewmodel/InsertViewModel.kt
@@ -30,3 +30,10 @@ class InsertViewModel(private val kontakRepository: KontakRepository) : ViewMode
 data class InsertUiState(
     val insertUiEvent: InsertUiEvent = InsertUiEvent(),
 )
+
+data class InsertUiEvent(
+    val id: Int = 0,
+    val nama: String = "",
+    val email: String = "",
+    val nohp: String = "",
+)
