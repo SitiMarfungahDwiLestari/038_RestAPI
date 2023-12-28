d94983280abbbcf81be67b6d33826de7b80a1c11
Siti Marfungah Dwi Lestari <siti.marfungah.ft21@mail.umy.ac.id>
2023-12-28 16:47:39 +0700

    menambahkan function untuk top bar

--- app/src/main/java/com/example/consumerestapi/ui/home/KontakApp.kt
+++ app/src/main/java/com/example/consumerestapi/ui/home/KontakApp.kt
@@ -2,13 +2,16 @@ package com.example.consumerestapi.ui.home
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
@@ -28,7 +31,8 @@ fun KontakApp(
     val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
     Scaffold (
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-        topBar = { TopAppBar(scrollBehavior = scrollBehavior)}
+        topBar = {
+            TopAppBarKontak(scrollBehavior = scrollBehavior) }
     ){
         Surface (
             modifier = Modifier
@@ -43,8 +47,24 @@ fun KontakApp(
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TopAppBar(scrollBehavior: TopAppBarScrollBehavior, modifier: Modifier = Modifier){
-    CenterAlignedTopAppBar(title = {Text(text = stringResource(R.string.app_name),
-        style = MaterialTheme.typography.headlineSmall)},
-        modifier = modifier)
+fun TopAppBarKontak(
+    title: String,
+    canNavigateBack: Boolean,
+    scrollBehavior: TopAppBarScrollBehavior,
+    modifier: Modifier = Modifier,
+    navigateUp: () -> Unit = {}
+){
+    CenterAlignedTopAppBar(
+        title = {Text(text = stringResource(R.string.app_name))},
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if(canNavigateBack){
+                IconButton(onClick = navigateUp){
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "")
+                }
+            }
+        })
 }
